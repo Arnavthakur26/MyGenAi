188c071968f1738d11b6cd08597e63951f8ea586
Arnav <arnav206@gmail.com>
2023-05-04 01:21:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Get the modal element
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal with animation
+btn.onclick = function () {
+  modal.style.display = "block";
+  modal.classList.add("fadeIn");
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.classList.remove("fadeIn");
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.classList.remove("fadeIn");
+    modal.style.display = "none";
+  }
+};
